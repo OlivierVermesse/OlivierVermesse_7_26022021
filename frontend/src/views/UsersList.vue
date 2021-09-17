5e2f6a66b0e07f85ecefa994444bd6dc2773c0ed
OlivierVermesse <olivier.vermesse74@gmail.com>
2021-09-17 14:19:49 +0200

    responsaive userList page

--- frontend/src/views/UsersList.vue
+++ frontend/src/views/UsersList.vue
@@ -1,31 +1,79 @@
 <template>
-    <main class="container">  
+    <div class="container">  
         <Home class = "mb-3"></Home>   
-        <div class="col-12">
-            <div class="col-12">
-               <router-link v-if="isAdmin" to='/Admin'><h1 class="my-2 btn btn-block btn-danger font-weight-bold">Page administrateur</h1></router-link>
-            </div>
+        <main class="col-12">
+            <section class="col-8 mx-auto">
+                <h1
+                class="my-2 btn btn-block btn-danger font-weight-bold adminTitle"
+                style="cursor: default"
+                >
+                Page administrateur
+                </h1>
+            </section>
             <section class="row">
-                <div class="col-12">
-                    <article class="card bg-light my-3 class=center-block" style="float:none;">
-                        <div class="card">
-                            <div class="row justify-content-around">
-                                <div class="m-1 my-auto"> Bonjour {{ nameCurrentUser }} ! </div>
-                                <button class="my-auto bg-white border border-white" @click="localClear"> <img class="bg-white border border-white" src="../assets/logout.png" alt="log-out" style="width:35px"/> </button>
+                <div class="col-12 col-md-4">
+                    <article class="card bg-light my-3 class=center-block" style="float: none" >
+                        <div class="card-header col-12">
+                            <div class="row justify-content-around bg-white">
+                                <div class="col-12 m-1 text-center">
+                                Bonjour {{ nameCurrentUser }} !
+                                </div>
+                                <button
+                  class="border border-white bg-white"
+                  @click="localClear"
+                >
+                  <img
+                    class="bg-white border border-white"
+                    src="../assets/logout.png"
+                    alt="log-out"
+                    style="width: 35px"
+                  />
+                </button>
+                                <div class="card-body text-center" v-if="isAdmin">
+                                    <p
+                                        v-if="isAdmin"
+                                        type="button"
+                                        class="btn btn-danger mx-auto rounded p-2"
+                                        style="cursor: default"
+                                    >
+                                        Vous êtes le modérateur
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                                <div class="text-center" style="padding-top:10px;padding-buttom:0" v-if="isAdmin">
-                                    <p v-if="isAdmin" type="button"  class=" btn btn-danger mx-auto rounded p-2" style="cursor:default">Vous êtes le modérateur</p> 
-                                </div>
-                        <div class="text-center" style="padding-top:0;padding-buttom:10px">
-                            <p class="btn rounded text-white font-weight-bold" style="background-color: green;cursor:default">Cet écran vous permet d'afficher ou supprimer les utilisateurs et les messages.</p>
-                        </div>
-                    </article> 
-                </div>    
-                <div class="col col-md-12">
-                    <h2 class="text-center m-5 ">
+                    </article>
+                    <div class="mx-auto">
+                        <p
+                        class="btn rounded text-white font-weight-bold"
+                        style="background-color: green; cursor: default"
+                        >
+                        Cet écran vous permet d'afficher ou supprimer les utilisateurs et
+                        les messages.
+                        </p>
+                    </div>
+                </div>
+        
+                <div class="col-12 col-md-12">
+                    <h2 class="text-center">
                         Liste des utilisateurs
                     </h2>
+
+                    <div class="listUser" v-for="i in users" :key="i">   
+                        <div class=" p-3">
+                            Id : {{ i.id }}
+                            <br>
+                            Nom : {{ i.lastName.charAt(0).toUpperCase() + i.lastName.slice(1) }}
+                            <br>
+                            Prénom : {{ i.userName.charAt(0).toUpperCase() + i.userName.slice(1) }}
+                            <br>
+                            Email : {{ i.email }}
+                            <br>
+                            Créé le : {{ i.createdAt.slice(0,10).split("-").reverse().join(".")}} 
+                            <br>
+                            Delete :  <button class="rounded" @click="deleteOneUser( i.id, isAdmin )"> <img src="../assets/delete.jpg" alt="delete" style="width:25px"> </button>
+                        </div>
+                    </div>
+
                     <table class="table table-striped">
                             <tr>
                                 <th class="py-3 text-center">Id</th>
@@ -36,32 +84,32 @@
                                 <th class="py-3 text-center">Supprimer</th>
                             </tr>
                             <tr>
-                                <td>
+                                <td class="text-center">
                                     <span class="badge p-3 badgeList" v-for="i in users" :key="i">   
                                          {{ i.id }}
                                     </span>
                                 </td>
-                                <td>
+                                <td class="text-center">
                                     <span class="badge p-3 badgeList" v-for="i in users" :key="i">   
                                          {{ i.lastName.charAt(0).toUpperCase() + i.lastName.slice(1) }}  
                                     </span>
                                 </td>
-                                <td>
+                                <td class="text-center">
                                     <span class="badge p-3 badgeList" v-for="i in users" :key="i">   
                                          {{ i.userName.charAt(0).toUpperCase() + i.userName.slice(1) }} 
                                     </span>
                                 </td>
-                                <td>
+                                <td class="text-center">
                                     <span class="badge p-3 badgeList" v-for="i in users" :key="i">   
                                          {{ i.email }}
                                     </span>
                                 </td>
-                                <td>
+                                <td class="text-center">
                                     <span class="badge p-3 badgeList" v-for="i in users" :key="i">   
                                         {{ i.createdAt.slice(0,10).split("-").reverse().join(".")}}
                                     </span>
                                 </td>
-                                <td>
+                                <td class="text-center col-1">
                                     <span class="badge p-2 badgeList" v-for="i in users" :key="i">   
                                         <button class="rounded bg-white border border-white" @click="deleteOneUser( i.id, isAdmin )"> <img src="../assets/delete.jpg" alt="deleteUser" style="width:25px"> </button>
                                     </span>
@@ -70,8 +118,8 @@
                     </table>
                 </div>
             </section>
-        </div>
-    </main>
+        </main>
+    </div>
 </template>
 
 <script>
