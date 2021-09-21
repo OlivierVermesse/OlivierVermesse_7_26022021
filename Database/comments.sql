a86afc625ce59b81d9b3bf6845f8e0cea6bdaa12
OlivierVermesse <olivier.vermesse74@gmail.com>
2021-09-21 19:49:09 +0200

    source database

---
+++ Database/comments.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM groupomania_p7.comments
+LIMIT 0, 1000
+
+-- Date: 2021-09-21 19:27
+*/
+INSERT INTO `` (`id`,`comment`,`createdAt`,`updatedAt`,`UserId`,`MessageId`,`createdAt`,`updatedAt`) VALUES (10,'ah quelle chance d\'avoir pu faire ce voyage','2021-09-21 18:55:20','2021-09-21 18:55:20',8,14,'2021-09-20 10:32:13','2021-09-20 10:32:13');
+INSERT INTO `` (`id`,`comment`,`createdAt`,`updatedAt`,`UserId`,`MessageId`,`createdAt`,`updatedAt`) VALUES (11,'oui mais dommage que qu\'on ne puisse pas mettre de photo avec un commentaire','2021-09-21 18:57:17','2021-09-21 18:57:17',3,34,'2021-09-21 18:52:46','2021-09-21 18:52:46');
+INSERT INTO `` (`id`,`comment`,`createdAt`,`updatedAt`,`UserId`,`MessageId`,`createdAt`,`updatedAt`) VALUES (12,'n\'hésitez pas à venir nous voir pour que l\'on vous explique','2021-09-21 18:58:49','2021-09-21 18:58:49',1,35,'2021-09-21 18:55:00','2021-09-21 18:55:00');
